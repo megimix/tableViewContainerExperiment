1c07c0ce70d289fc1a0fc264fac6d3a516dae149
Tal Shachar <tal.s@fiverr.com>
2015-06-03 09:19:56 +0300

    Initial Commit

---
+++ ContainterTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ContainterTest
+//
+//  Created by Tal Shachar on 6/3/15.
+//  Copyright (c) 2015 Tal Shachar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
